87a0da0912160f1f8bb1cdb33f318f0755e7693f
bby0 <nuanbaobao123@Gmail.com>
2021-07-02 15:05:38 +0800

    Update bilibili.py

--- function/bilibili/bilibili.py
+++ function/bilibili/bilibili.py
@@ -324,11 +324,10 @@ class BiliBiliCheckIn(object):
             if silver2coin:
                 silver2coin_ret = self.silver2coin(session=session, bili_jct=bili_jct)
                 print(silver2coin_ret)
-                print(11111111111)
                 if silver2coin_ret["code"] == 0:
                     silver2coin_msg = f"成功将银瓜子兑换为1个硬币"
                 else:
-                    silver2coin_msg = silver2coin_ret["msg"]
+                    silver2coin_msg = silver2coin_ret["message"]
                 print(silver2coin_msg)
             else:
                 silver2coin_msg = f"未开启银瓜子兑换硬币功能"
